7e830c3ef582f1f10a8c4af1a53902cdf6a7922c
markveligod <markveligod@yandex.ru>
2020-12-30 10:30:13 +0300

    fix class server

--- sources/Class.Server.cpp
+++ sources/Class.Server.cpp
@@ -6,12 +6,6 @@ Server::Server(const int & fd, const std::string & servername, const int & hopco
     this->_server_name = servername;
     this->_hopcount = hopcount;
     this->_info = info;
-
-    std::cout << "FD: " << fd << std::endl;
-    std::cout << "servername: " << servername << std::endl;
-    std::cout << "hopcount: " << hopcount << std::endl;
-    std::cout << "info: " << info << std::endl;
-    std::cout << "DONE!" << std::endl;
 }
 
 Server::~Server()
