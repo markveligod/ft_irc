76af7af094b445969d3d51e86a86cd44645047b0
poringdol <sashe@bk.ru>
2021-01-16 22:05:24 +0300

    JOIN, PART done

--- sources/commands/join.cpp
+++ sources/commands/join.cpp
@@ -53,18 +53,6 @@ join_channel(IRC& irc,
 			User* user,
 			int fd)
 {
-	if (is_server(irc, fd))											// check if join recieved by server 
-	{
-		join_from_server(irc, user, channel_name);
-
-		string message1 = irc.full_name(user) + " JOIN :" + channel_name;
-		string message2 = ":" + user->getName() + " JOIN :" + channel_name;
-
-		irc.forward_message_to_channel(fd, channel_name, message1);
-		irc.forward_message_to_servers(fd, message2, true);
-		return;
-	}
-
 	if (!Channel::is_valid_channel_name(channel_name))				// check channel name
 	{
 		irc.push_cmd_queue(fd, irc.response(ERR_NOSUCHCHANNEL, fd, channel_name, ERR_NOSUCHCHANNEL_MESS));
@@ -82,79 +70,66 @@ join_channel(IRC& irc,
 		channels[channel_name].add_user(user);
 		channels[channel_name].set_operator(user);
 
-		irc.push_cmd_queue(fd, message1 + "\r\n");
+		if (!irc.is_server(fd))
+			irc.push_cmd_queue(fd, message1 + "\r\n");
+		
+		irc.forward_message_to_channel(fd,channel_name, message1);
 		irc.forward_message_to_servers(fd, message2, true);
+	
+		string mode_message = irc.full_name(user) + " MODE :" + channel_name + " +o " + user->getName() + "\r\n";
+		irc.forward_message_to_channel(fd, channel_name, mode_message);
+		irc.forward_message_to_servers(fd, mode_message, true);
 
 		utils::print_line("Channel " + channel_name + " created");
 		utils::print_line("User " + user->getNickname() + " joined channel " + channel_name);
 	}
 	else															// channel already exist
 	{
-		if (channels[channel_name].is_user_in_channel(user))		// check if user already in channel
+		Channel& channel = channels[channel_name];
+
+		if (channel.is_user_in_channel(user))		// check if user already in channel
 			return;
 
-		if (channels[channel_name].is_banned(user))					// check if user is banned 
+		if (channel.is_banned(user))					// check if user is banned 
 		{
 			irc.push_cmd_queue(fd, irc.response(ERR_BANNEDFROMCHAN, fd, channel_name, ERR_BANNEDFROMCHAN_MESS));
 			return;
 		}
 
-		if (!channels[channel_name].is_valid_key(channel_key))		// check channel password
+		if (!channel.is_valid_key(channel_key))		// check channel password
 		{
 			irc.push_cmd_queue(fd, irc.response(ERR_BADCHANNELKEY, fd, channel_name, ERR_BADCHANNELKEY_MESS));
 			return;
 		}
 
-		if (channels[channel_name].is_invite_only())				// check if invite only channel
+		if (channel.is_invite_only())				// check if invite only channel
 		{
 			irc.push_cmd_queue(fd, irc.response(ERR_INVITEONLYCHAN, fd, channel_name, ERR_INVITEONLYCHAN_MESS));
 			return;
 		}
 
-		utils::print_line("Channel exists");
-		channels[channel_name].add_user(user);
+		channel.add_user(user);
 
-		irc.push_cmd_queue(fd, message1 + "\r\n");
+		if (!irc.is_server(fd))
+		{
+			irc.push_cmd_queue(fd, message1 + "\r\n");
+			if (channel.get_topic().size())
+				send_topic(irc, fd, channel_name, channel.get_topic());	// отправляем Топик
+		}
 		irc.forward_message_to_channel(fd, channel_name, message1);
 		irc.forward_message_to_servers(fd, message2, true);
 
-		send_topic(irc, fd, channel_name, channels[channel_name].get_topic());	// отправляем Топик
 
 		// std::cout << "DEBUG Channel members list:\n";
 		// channels[channel_name].print_users();
 	}
-	user->inc_channel_count();										// увеличиваем количество каналов, в которых состоит пользователь
+	user->inc_channel_count();											// увеличиваем количество каналов, в которых состоит пользователь
 	
-	send_channel_users(irc, fd, user, channels[channel_name]);		// отсылаем подключившемуся список всех пользователей канала
-	irc.push_cmd_queue(fd, irc.response(RPL_ENDOFNAMES, fd, channel_name, RPL_ENDOFNAMES_MESS));
-	
-	//irc.print_channels(); //DEBUG
+	if (!irc.is_server(fd))
+	{
+		send_channel_users(irc, fd, user, channels[channel_name]);		// отсылаем подключившемуся список всех пользователей канала
+		irc.push_cmd_queue(fd, irc.response(RPL_ENDOFNAMES, fd, channel_name, RPL_ENDOFNAMES_MESS));
+	}
 	
 	return;
-}
-
-int Command::
-join_from_server(IRC& irc, User* user, const string& channel_name)
-{
-	channel_map& channels = irc.get_channels();
-
-	if (channels.count(channel_name))
-		channels[channel_name].add_user(user);
-	else
-	{
-		channels.insert(make_pair(channel_name, Channel(channel_name, string(), user)));
-
-		channels[channel_name].add_user(user);
-		channels[channel_name].set_operator(user);
-		utils::print_line("Channel " + channel_name + " created");
-	}
-	user->inc_channel_count();
-
-	utils::print_line("User " + user->getNickname() + " joined channel " + channel_name);
-
-	// std::cout << "DEBUG Channel members list:\n";
-	// channels[channel_name].print_users();
-
-
-	return 0;
 }
